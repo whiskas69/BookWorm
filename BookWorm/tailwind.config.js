80d95bbe5eab4a40dda962adc8da508970f7e320
mymoon69 <64070105@it.kmitl.ac.th>
2024-03-03 16:54:43 +0700

    first

---
+++ BookWorm/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      navbar: '#B05044'
+    },
+    borderRadius: {
+      'navbar': '60px',
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
