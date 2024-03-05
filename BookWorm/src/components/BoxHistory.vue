6d203ff03deac3a1363c7a41e47b72194bd682d7
whiskas69 <64070075@it.kmail.ac.th>
2024-03-05 21:02:47 +0700

    history

---
+++ BookWorm/src/components/BoxHistory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="inputCoin flex flex-row items-center box-content h-32 w-full p-3 border-solid border-2 border-neutral-100 rounded-[30px] drop-shadow-lg">
+    <div class="coverBook">
+        <img
+      class="loginImg w-[80px] rounded-lg ml-8"
+      src="https://cdn.discordapp.com/attachments/979676845228318740/1018546182768836648/Imagenes_de_Coco_-_Imector.jpeg?ex=65f70e8d&is=65e4998d&hm=780f2ffa8091385150b6dcd4ddf8b7b8ee535ea47fafde74d6cb7cab6e30a859&"
+      alt="coverBook"
+    />
+    </div>
+    <div>
+        <label class="coin text-2xl ml-20">Book Name Der</label>
+    </div>
+    <div>
+        <label class="coin text-xl ml-[280px]">10-01-1010</label>
+    </div>
+    <div>
+        <label class="coin text-xl ml-[200px]">ชำระเสร็จสิ้น</label>
+    </div>
+    <div>
+        <label class="coin text-xl ml-[300px]">10 บาท</label>
+    </div>
+  </div>
+</template>
