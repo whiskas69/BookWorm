fb05b8f5ad1663226b157a2993e6563bbf37dea8
mymoon69 <64070105@it.kmitl.ac.th>
2024-03-12 17:22:21 +0700

    l

--- BookWorm/front-end/src/views/RegisterView.vue
+++ BookWorm/front-end/src/views/RegisterView.vue
@@ -1,116 +1,123 @@
 <template>
   <div class="flex min-h-full flex-1 flex-col px-6 lg:px-8">
     <div class="hero-body">
-        <div class="columns is-centered">
-          <div class="flex justify-center gap-x-6 py-2">
-            <!-- login -->
-            <!-- img -->
-            <div class="flex min-w-0 gap-x-4">
-              <img class="loginImg bg-cover" src="@/assets/5294.jpg" alt="Login image" />
+      <div class="columns is-centered">
+        <div class="flex justify-center gap-x-6 py-2">
+          <!-- login -->
+          <!-- img -->
+          <div class="flex min-w-0 gap-x-4">
+            <img
+              class="loginImg bg-cover"
+              src="@/assets/5294.jpg"
+              alt="Login image"
+            />
+          </div>
+
+          <div class="loginData min-w-0">
+            <div class="mt-20">
+              <label class="title">Welcome to the Land of Writing!</label>
             </div>
 
-            <div class="loginData min-w-0">
-              <div class="mt-20">
-                <label class="title">Welcome to the Land of Writing!</label>
-              </div>
+            <div class="mt-4">
+              <label class="subtitle"
+                >Sign up today and start your journey as a bookworm!</label
+              >
+            </div>
 
-              <div class="mt-4">
-                <label class="subtitle"
-                  >Sign up today and start your journey as a bookworm!</label
-                >
+            <!-- user -->
+            <div class="field mt-8">
+              <label class="label">Username</label>
+              <div class="mt-2">
+                <input
+                  id="Username"
+                  name="Username"
+                  type="Username"
+                  v-model="username"
+                  required=""
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
+            </div>
+            <!-- email -->
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="mt-2">
+                <input
+                  id="Email"
+                  name="Email"
+                  type="Email"
+                  v-model="email"
+                  required=""
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <!-- password -->
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="mt-2">
+                <input
+                  id="Password"
+                  name="Password"
+                  type="Password"
+                  v-model="password"
+                  required=""
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <!-- password -->
+            <div class="field">
+              <label class="label">Confirmed Password</label>
+              <div class="mt-2">
+                <input
+                  id="Password"
+                  name="Password"
+                  type="Password"
+                  v-model="conpassword"
+                  required=""
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <!-- Display Name -->
+            <div class="field">
+              <label class="label">Display Name</label>
+              <div class="mt-2">
+                <input
+                  id="Display Name"
+                  name="Display Name"
+                  type="Display Name"
+                  v-model="displayname"
+                  required=""
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-              <!-- user -->
-              <div class="field mt-8">
-                <label class="label">Username</label>
-                <div class="mt-2">
-                  <input
-                    id="Username"
-                    name="Username"
-                    type="Username"
-                    autocomplete="Username"
-                    required=""
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <!-- email -->
-              <div class="field">
-                <label class="label">Email</label>
-                <div class="mt-2">
-                  <input
-                    id="Email"
-                    name="Email"
-                    type="Email"
-                    autocomplete="Email"
-                    required=""
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <!-- password -->
-              <div class="field">
-                <label class="label">Password</label>
-                <div class="mt-2">
-                  <input
-                    id="Password"
-                    name="Password"
-                    type="Password"
-                    autocomplete="Password"
-                    required=""
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <!-- password -->
-              <div class="field">
-                <label class="label">Confirmed Password</label>
-                <div class="mt-2">
-                  <input
-                    id="Password"
-                    name="Password"
-                    type="Password"
-                    autocomplete="Password"
-                    required=""
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <!-- Display Name -->
-              <div class="field">
-                <label class="label">Display Name</label>
-                <div class="mt-2">
-                  <input
-                    id="Display Name"
-                    name="Display Name"
-                    type="Display Name"
-                    autocomplete="Display Name"
-                    required=""
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  class="submit flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Create Account
-                </button>
-              </div>
-              <p class="mt-5 text-center text-sm text-gray-500">
-                Already have an account?
-                {{ " " }}
-                <a
-                  href="#"
+            <div>
+              <button
+                type="submit"
+                @click="submit()"
+                class="submit flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Create Account
+              </button>
+            </div>
+            <p class="mt-5 text-center text-sm text-gray-500">
+              Already have an account?
+              {{ " " }}
+              <router-link to="/login">
+                <p
                   class="signUp font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                  >Sign In</a
                 >
-              </p>
-            </div>
+                  Sign In
+                </p>
+              </router-link>
+            </p>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -118,7 +125,46 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      username: "",
+      password: "",
+      conpassword: "",
+      email: "",
+      displayname: "",
+    };
+  },
+  methods: {
+    submit() {
+      if (
+        this.password === this.conpassword &&
+        this.password != "" &&
+        this.conpassword != "" &&
+        this.email != "" &&
+        this.username != "" &&
+        this.displayname != ""
+      ) {
+        const data = {
+          email: this.email,
+          password: this.password,
+          username: this.username,
+          displayname: this.displayname,
+        };
+
+        axios
+          .post("http://52.86.167.253:3000/register", data)
+          .then((res) => {
+            this.err = "";
+            console.log(res);
+            alert("You are already our mrmber!");
+            this.$router.push({ path: "/login" });
+          })
+          .catch((err) => {
+            this.err = err.response.data.details.message;
+            console.log(err);
+            alert(err.response.data.details.message);
+          });
+      }
+    },
   },
 };
 </script>
@@ -140,7 +186,7 @@ export default {
   background: #86a5a8;
 }
 .loginData {
-margin-left: -10%;
+  margin-left: -10%;
 }
 .title {
   color: #2f4842;
@@ -155,6 +201,6 @@ margin-left: -10%;
   margin-top: 5%;
 }
 .signUp {
-  color: #B05044;
+  color: #b05044;
 }
 </style>
